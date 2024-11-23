57775570f12c38d71fa3c6d0e63cc4e1569cf0b4
islam khalid <201045@ppu.edu.ps>
2024-11-23 19:50:35 +0200

    commit

--- dist/task4_5.js
+++ dist/task4_5.js
@@ -1,5 +1,5 @@
 "use strict";
-console.log('------------task 4------------');
+console.log('\n ------------task 4------------ \n');
 const products = [
     { name: 'phone', price: 4000 },
     { name: "book", price: 20 },
@@ -11,14 +11,18 @@ const calc_total_price = (product) => {
 const totalPrice = calc_total_price(products);
 console.log(`Total product price = ${totalPrice}`);
 ////////////////////////////////////
-console.log('------------task 5------------');
+console.log(` \n ------------task 5------------ \n`);
 const is_valid_email = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 };
 const email1 = "islam@gmail.com";
-console.log(`${email1} is valid ? ${is_valid_email(email1)}`); // Output: true
-console.log(is_valid_email("invalid")); // Output: false
-console.log(is_valid_email("is@@@domain.com")); // Output: false
-console.log(is_valid_email("ali@domain")); // Output: false
-console.log(is_valid_email("someone@gmail.ss.ss")); // Output: true
+const email2 = "invalid";
+const email3 = "is@@@domain.com";
+const email4 = "ali@domain";
+const email5 = "someone@gmail.ss.ss";
+console.log(`___ ${email1} ___ is valid ? ${is_valid_email(email1)}`); // Output: true
+console.log(`___ ${email2} ___ is valid ? ${is_valid_email(email2)}`); // Output: false
+console.log(`___ ${email3} ___ is valid ? ${is_valid_email(email3)}`); // Output: false
+console.log(`___ ${email4} ___  is valid ? ${is_valid_email(email4)}`); // Output: false
+console.log(`___ ${email5} ___ is valid ? ${is_valid_email(email5)}`); // Output: true
